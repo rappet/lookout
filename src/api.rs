87407aa1d3c0ae735d2864426afe799b388aa5c4
Raphael Peters <rappet@rappet.de>
2019-10-07 00:42:11 +0200

    Changed mtr parameters

--- src/api.rs
+++ src/api.rs
@@ -34,7 +34,7 @@ impl Params {
                 Ok(Some(String::from_utf8(output)?))
             },
             (Some(Target::Ip(ip)), Command::Mtr) => {
-                let output = process::Command::new("mtr").args(&["-we", &ip.to_string()]).output()?.stdout;
+                let output = process::Command::new("mtr").args(&["-wenc3", &ip.to_string()]).output()?.stdout;
                 Ok(Some(String::from_utf8(output)?))
             },
             _ => Ok(None)
